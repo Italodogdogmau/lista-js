1b46e2cc970afd78132c944aef0218d5ef49f1bf
Italo Rocha <33815415+Italodogdogmau@users.noreply.github.com>
2019-03-14 17:57:14 -0300

    Add files via upload

---
+++ atividade3.js
@@ -0,0 +1,9 @@
+	function mudaCor(cor) {
+		switch(cor){
+			case'Azul': document.bgColor = "blue";break;
+			case'Lilas': document.bgColor = "plum";break;
+			case'marrom': document.bgColor = "brown";break;
+			case'roxo': document.bgColor = "mediumPurple";break;
+
+			}
+		}
